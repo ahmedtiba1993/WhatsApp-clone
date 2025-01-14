aa61a89eaa016a91de8ef46de3a0de7b2ba5d5f2
Ahmed Tiba <ahmed.tiba.1993@gmail.com>
2025-01-14 19:08:28 +0100

    sychronize the users with keycloak

--- whatsapp-api/src/main/java/com/tiba/whatsapp_api/user/UserRepository.java
+++ whatsapp-api/src/main/java/com/tiba/whatsapp_api/user/UserRepository.java
@@ -17,4 +17,5 @@ public interface UserRepository extends JpaRepository<User, String> {
 
     @Query("SELECT u FROM User u WHERE u.id = :publicId")
     Optional<User> findUserByPublicId(@Param("publicId") String publicId);
+
 }
